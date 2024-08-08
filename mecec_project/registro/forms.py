8ee289c27035b534be299f1571376c72cc47a1eb
Franco <fpalossi@gmail.com>
2024-08-08 18:25:59 -0300

    cambios en el estilo css

--- mecec_project/registro/forms.py
+++ mecec_project/registro/forms.py
@@ -2,13 +2,27 @@ from django import forms
 from .models import Registro
 
 class RegistroForm(forms.ModelForm):
-    fecha_nac = forms.DateField(
-        input_formats=['%d/%m/%Y'],  # Ejemplo de formato DD/MM/YYYY
-        widget=forms.DateInput(attrs={
-            'placeholder': 'DD/MM/YYYY',
-            'class': 'form-control'
-        })
-    )
     class Meta:
         model = Registro
         fields = '__all__'
+        widgets = {
+            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombres': forms.TextInput(attrs={'class': 'form-control'}),
+            'dni': forms.TextInput(attrs={'class': 'form-control'}),
+            'fecha_nac': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'ciudad': forms.TextInput(attrs={'class': 'form-control'}),
+            'provincia': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre_apellido_paterno': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombre_apellido_materno': forms.TextInput(attrs={'class': 'form-control'}),
+            'celular': forms.TextInput(attrs={'class': 'form-control'}),
+            'domicilio_actual': forms.TextInput(attrs={'class': 'form-control'}),
+            'correo_electronico': forms.EmailInput(attrs={'class': 'form-control'}),
+            'foto': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'ministerio': forms.TextInput(attrs={'class': 'form-control'}),
+            'registro': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo_pabellon': forms.TextInput(attrs={'class': 'form-control'}),
+            'numero_pabellon': forms.TextInput(attrs={'class': 'form-control'}),
+            'seccion_pabellon': forms.TextInput(attrs={'class': 'form-control'}),
+            'otros_pabellon': forms.TextInput(attrs={'class': 'form-control'}),
+            'autorizacion_iglesia': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
