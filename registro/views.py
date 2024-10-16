4235750b5c16407312b0168f515ddaae5094b05c
Franco <fpalossi@gmail.com>
2024-10-16 13:43:44 -0300

    ordenado por ministario

--- registro/views.py
+++ registro/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistroForm, Registro 
 from django.contrib.auth.decorators import login_required
-
+from .models import Registro
 
 def home(request):
     return render(request, 'home.html')
@@ -24,7 +24,22 @@ def listado_registros(request):
     if query:
         registros = Registro.objects.filter(nombres__icontains=query)
     else:
-        registros = Registro.objects.all()
+        registros = Registro.objects.all().order_by('ministerio')
+        #filtros
+        nombre = request.GET.get('nombre')
+        apellido = request.GET.get('apellido')
+        dni = request.GET.get('dni')
+        activo = request.GET.get('activo')
+
+        if nombre:
+            registros = registros.filter(nombres__icontains=nombre)
+        if apellido:
+            registros = registros.filter(apellido__icontains=apellido)
+        if dni:
+            registros = registros.filter(dni__icontains=dni)
+        if activo is not None:
+            registros = registros.filter(activo=activo)
+        #end filtros
     return render(request, 'listado.html', {'registros': registros})
 
 def registro_detalle(request, id):
